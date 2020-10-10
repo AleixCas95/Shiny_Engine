49246e314c9cf7c9b5696640c0ddc843a73973e3
AitorVelez <aitorvelez@hotmail.com>
2020-10-10 16:17:10 +0200

    Added more menus

--- Shiny_Engine/ModuleGUI.cpp
+++ Shiny_Engine/ModuleGUI.cpp
@@ -45,31 +45,12 @@ bool ModuleGUI::CleanUp()
 
 update_status ModuleGUI::Update(float dt)
 {
-
-	//ImGui::ShowTestWindow();
-	//static bool show_test = false;
-	//static bool custom = false;
-	//static bool normal = true;
-
-	//GLint total_memory;
-	//GLint memory_usage = 0;
-	//GLint dedicated_memory = 0;
-	//GLint available_memory = 0;
-
-	//glGetIntegerv(GL_GPU_MEMORY_INFO_TOTAL_AVAILABLE_MEMORY_NVX, &total_memory);
-	//glGetIntegerv(GL_GPU_MEMORY_INFO_CURRENT_AVAILABLE_VIDMEM_NVX, &available_memory);
-	//glGetIntegerv(GL_GPU_MEMORY_INFO_DEDICATED_VIDMEM_NVX, &dedicated_memory);
-
-	//memory_usage = total_memory - available_memory;
-
-	//if (show_test)
-	//	ImGui::ShowTestWindow();
 	if (ImGui::BeginMainMenuBar())
 	{
 		if (ImGui::BeginMenu("Menu"))
 		{
 			if (ImGui::MenuItem("Open..", "Ctrl+O"))
-			if (ImGui::MenuItem("Save", "Ctrl+S"))
+			if (ImGui::MenuItem("Save", "Ctrl+S")) 
 			if (ImGui::MenuItem("Quit", "Alt+F4"))
 			{
 				return UPDATE_STOP;
@@ -77,15 +58,30 @@ update_status ModuleGUI::Update(float dt)
 
 			ImGui::EndMenu();
 		}
+		if (ImGui::BeginMenu("View"))
+		{
+		}
+
+		if (ImGui::BeginMenu("Help"))
+		{
+		}
 		ImGui::EndMainMenuBar();
 	}
 	if (ImGui::Begin("Configuration"))
 	{
 		if (ImGui::CollapsingHeader("Application"))
 		{
-		
+		}
+		if (ImGui::CollapsingHeader("File System"))
+		{
+		}
+		if (ImGui::CollapsingHeader("Input"))
+		{
 		}
 		if (ImGui::CollapsingHeader("Hardware"))
+		{
+		}
+		if (ImGui::CollapsingHeader("Options"))
 		{
 		}
 	}
