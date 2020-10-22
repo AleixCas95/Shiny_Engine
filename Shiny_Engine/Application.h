ab2fdc531696ca9df0799720691de1c78d00310a
AitorVelez <aitorvelez@hotmail.com>
2020-10-22 12:02:00 +0200

    glut was still needed

---
+++ Shiny_Engine/Application.h
@@ -0,0 +1,60 @@
+#pragma once
+
+
+#include "Globals.h"
+#include "Timer.h"
+#include "Module.h"
+#include "ModuleWindow.h"
+#include "ModuleInput.h"
+#include "ModuleScene.h"
+#include "ModuleRenderer3D.h"
+#include "ModuleCamera3D.h"
+#include "ModulePlayer.h"
+#include "ModuleGUI.h"
+#include "ModuleFBX.h"
+//#include "ModuleGeometry.h"
+#include <list>
+
+class Application
+{
+public:
+	ModuleWindow* window;
+	ModuleInput* input;
+	ModuleScene* scene;
+	ModuleRenderer3D* renderer3D;
+	ModuleCamera3D* camera;
+	//ModulePlayer* player;
+	ModuleGUI* gui;
+	ModuleFBX* fbx;
+	//ModuleGeometry* geometry;
+
+	char* app_name;
+	char* organization;
+
+private:
+
+	Timer	ms_timer;
+	float	dt = 0.0f;
+	float last_FPS = 0.0f;
+	float last_ms = 0.0f;
+	std::list<Module*> list_modules;
+
+
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+	float GetMS();
+	float GetFPS();
+
+private:
+
+	void AddModule(Module* mod);
+	void PrepareUpdate();
+	void FinishUpdate();
+};
