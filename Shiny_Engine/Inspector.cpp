ffdb6b5e77e4934069ed9c7799000708a6383b19
AleixCas95 <leix_05@hotmail.com>
2020-10-29 18:28:59 +0100

    Module inspector added

---
+++ Shiny_Engine/Inspector.cpp
@@ -0,0 +1,18 @@
+#include "Inspector.h"
+#include "Application.h"
+#include "ModuleScene.h"
+
+Inspector::Inspector()
+{
+}
+
+Inspector::~Inspector()
+{
+}
+
+void Inspector::Draw()
+{
+	
+	
+	ImGui::End();
+}
---
+++ Shiny_Engine/Inspector.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ModuleGui.h"
+#include "ImGui/imgui.h"
+
+class Inspector
+{
+public:
+	Inspector();
+	~Inspector();
+
+	void Draw();
+};
+
