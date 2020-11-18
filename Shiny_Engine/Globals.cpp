858c284be656d0edc76030ceb35d815698d26496
AleixCas95 <leix_05@hotmail.com>
2020-11-18 17:54:33 +0100

    Add Globals.cpp

---
+++ Shiny_Engine/Globals.cpp
@@ -0,0 +1,29 @@
+#include "Globals.h"
+
+#include <random>
+
+// ~~ FUNCTIONS ~~
+bool FileExist(const std::string& name)
+{
+	
+}
+
+std::string GetFileNameFromPath(const char* path)
+{
+
+}
+
+std::string GetFileNameWithExtension(const char* path)
+{
+
+}
+
+uint GenRandomNumber()
+{
+	
+}
+
+std::vector<std::string> ReadAllFilesOnPath(const char* path, const char* extension)
+{
+	
+}
