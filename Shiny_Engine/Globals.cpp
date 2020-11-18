84dbb02df3cbfef25ae5fc0aab834dc55184c84e
AleixCas95 <leix_05@hotmail.com>
2020-11-18 17:55:58 +0100

    Base globals added

--- Shiny_Engine/Globals.cpp
+++ Shiny_Engine/Globals.cpp
@@ -5,25 +5,43 @@
 // ~~ FUNCTIONS ~~
 bool FileExist(const std::string& name)
 {
-	
+	if (FILE* file = fopen(name.c_str(), "r"))
+	{
+		fclose(file);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 std::string GetFileNameFromPath(const char* path)
 {
+	std::string filename = path;
 
+	
+	return filename;
 }
 
 std::string GetFileNameWithExtension(const char* path)
 {
+	std::string filename = path;
 
+	return filename;
 }
 
 uint GenRandomNumber()
 {
+	uint number = 0;
 	
+
+	return number;
 }
 
 std::vector<std::string> ReadAllFilesOnPath(const char* path, const char* extension)
 {
+	std::vector<std::string> paths;
 	
+	return paths;
 }
