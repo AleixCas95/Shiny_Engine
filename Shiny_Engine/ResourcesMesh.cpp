966c20d9cbbde52d0e64b0aa6b93790fcebb6891
AleixCas95 <leix_05@hotmail.com>
2020-12-09 20:31:55 +0100

    resourcesMesh

--- Shiny_Engine/ResourcesMesh.cpp
+++ Shiny_Engine/ResourcesMesh.cpp
@@ -1,2 +1,24 @@
 #include "ResourcesMesh.h"
 #include "Glew/include/glew.h"
+
+ResourceMesh::ResourceMesh(const char* path) : Resource(ResourceType::Mesh, path)
+{
+}
+
+ResourceMesh::~ResourceMesh()
+{
+	glDeleteBuffers(1, (GLuint*) & (index.id));
+	glDeleteBuffers(1, (GLuint*) & (vertex.id));
+	glDeleteBuffers(1, (GLuint*) & (normals.id));
+	glDeleteBuffers(1, (GLuint*) & (uvs.id));
+
+	delete index.data;
+	delete vertex.data;
+	delete normals.data;
+	delete uvs.data;
+}
+
+void ResourceMesh::Unload()
+{
+	/// TODO
+}
--- Shiny_Engine/ResourcesMesh.h
+++ Shiny_Engine/ResourcesMesh.h
@@ -1,2 +1,33 @@
 #pragma once
 #include "Resources.h"
+
+
+class GameObject;
+
+template <typename T>
+struct buffer
+{
+	uint id = 0u;
+	uint size = 0u;
+	T* data = nullptr;
+};
+
+class ResourceMesh : public Resource
+{
+public:
+	ResourceMesh(const char* path);
+	~ResourceMesh();
+
+	void Unload();
+
+public:
+
+	int id = -1;
+	buffer<unsigned int> index;
+	buffer<float> vertex;
+	buffer<float> normals;
+	buffer<float> uvs;
+
+	bool hasNormals = false;
+};
+
