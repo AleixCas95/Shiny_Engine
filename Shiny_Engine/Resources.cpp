84fcf0f6dc6981e4003e45e111eb17a4cbcb82fa
AleixCas95 <leix_05@hotmail.com>
2020-12-09 20:31:20 +0100

    Filling Resources

--- Shiny_Engine/Resources.cpp
+++ Shiny_Engine/Resources.cpp
@@ -1,2 +1,20 @@
 #include "Resources.h"
 #include "Application.h"
+
+Resource::Resource(ResourceType type, const char* path) : type(type), name(path)
+{
+}
+
+Resource::~Resource()
+{
+}
+
+bool Resource::operator==(Resource other)
+{
+	if (this->type == other.type)
+	{
+		if (strcmp(this->name.data(), other.name.data()) == 0)
+			return true;
+	}
+	return false;
+}
--- Shiny_Engine/Resources.h
+++ Shiny_Engine/Resources.h
@@ -1,3 +1,27 @@
 #pragma once
 #include "Globals.h"
 #include <string>
+
+enum class ResourceType
+{
+	None = 0,
+	Mesh,
+	Texture,
+	Scene
+};
+
+class Resource
+{
+public:
+	Resource(ResourceType type, const char* path);
+	virtual ~Resource();
+
+	virtual void Unload() {};
+
+	bool operator==(Resource other);
+
+public:
+	std::string name;
+	ResourceType type = ResourceType::None;
+	int usage = 0;
+};
