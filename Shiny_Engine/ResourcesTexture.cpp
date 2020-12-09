b4ff6ea396e5d748da788c00a6c0a5c4eb2d8424
AleixCas95 <leix_05@hotmail.com>
2020-12-09 20:32:34 +0100

    ResourcesTexture

--- Shiny_Engine/ResourcesTexture.cpp
+++ Shiny_Engine/ResourcesTexture.cpp
@@ -1,2 +1,16 @@
 #include "ResourcesTexture.h"
 #include "Glew/include/glew.h"
+
+ResourceTexture::ResourceTexture(const char* path) : Resource(ResourceType::Texture, path)
+{
+}
+
+ResourceTexture::~ResourceTexture()
+{
+	glDeleteTextures(1, (GLuint*) & (id));
+}
+
+void ResourceTexture::Unload()
+{
+	
+}
--- Shiny_Engine/ResourcesTexture.h
+++ Shiny_Engine/ResourcesTexture.h
@@ -1,2 +1,15 @@
 #pragma once
 #include "Resources.h"
+
+class ResourceTexture :
+	public Resource
+{
+public:
+	ResourceTexture(const char* path);
+	~ResourceTexture();
+
+	void Unload();
+
+public:
+	uint id = 0u;
+};
