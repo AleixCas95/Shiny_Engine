39cfc6657cef365fb4c712ebed4888dcd935275a
AitorVelez <aitorvelez@hotmail.com>
2020-12-13 01:10:08 +0100

    frustum lines

--- Shiny_Engine/ModuleRenderer3D.cpp
+++ Shiny_Engine/ModuleRenderer3D.cpp
@@ -320,6 +320,56 @@ update_status ModuleRenderer3D::PostUpdate(float dt)
 			}
 		}
 	}
+
+	if (App->scene->current_object)
+	{
+		if (App->scene->current_object->active)
+		{
+			ComponentCamera* cam = (ComponentCamera*)App->scene->current_object->GetComponent(Object_Type::CompCamera);
+			if (cam)
+			{
+				cam->UpdateFrustum();
+				float3 corners[8];
+				cam->frustum.GetCornerPoints(corners);
+
+				glLineWidth(2.0f);
+				glColor3f(0.0f, 1.0f, 0.0f);
+				glBegin(GL_QUADS);
+
+				glVertex3fv((GLfloat*)&corners[1]);
+				glVertex3fv((GLfloat*)&corners[5]);
+				glVertex3fv((GLfloat*)&corners[7]);
+				glVertex3fv((GLfloat*)&corners[3]);
+
+				glVertex3fv((GLfloat*)&corners[4]);
+				glVertex3fv((GLfloat*)&corners[0]);
+				glVertex3fv((GLfloat*)&corners[2]);
+				glVertex3fv((GLfloat*)&corners[6]);
+
+				glVertex3fv((GLfloat*)&corners[5]);
+				glVertex3fv((GLfloat*)&corners[4]);
+				glVertex3fv((GLfloat*)&corners[6]);
+				glVertex3fv((GLfloat*)&corners[7]);
+
+				glVertex3fv((GLfloat*)&corners[0]);
+				glVertex3fv((GLfloat*)&corners[1]);
+				glVertex3fv((GLfloat*)&corners[3]);
+				glVertex3fv((GLfloat*)&corners[2]);
+
+				glVertex3fv((GLfloat*)&corners[3]);
+				glVertex3fv((GLfloat*)&corners[7]);
+				glVertex3fv((GLfloat*)&corners[6]);
+				glVertex3fv((GLfloat*)&corners[2]);
+
+				glVertex3fv((GLfloat*)&corners[0]);
+				glVertex3fv((GLfloat*)&corners[4]);
+				glVertex3fv((GLfloat*)&corners[5]);
+				glVertex3fv((GLfloat*)&corners[1]);
+
+				glEnd();
+			}
+		}
+	}
 	return UPDATE_CONTINUE;
 }
 
