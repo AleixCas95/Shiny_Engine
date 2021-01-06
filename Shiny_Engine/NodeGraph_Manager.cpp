0595f0282f5f624998057a5e99de3db3ffe8e24e
AitorVelez <aitorvelez@hotmail.com>
2021-01-06 18:38:12 +0100

    Empty NodeGraph modules

---
+++ Shiny_Engine/NodeGraph_Manager.cpp
@@ -0,0 +1,5 @@
+#include "NodeGraph_Manager.h"
+
+
+NodeGraph_Manager::NodeGraph_Manager() {}
+NodeGraph_Manager::~NodeGraph_Manager() {}
---
+++ Shiny_Engine/NodeGraph_Manager.h
@@ -0,0 +1,16 @@
+#ifndef __NodeGraph_Manager_H__
+#define __NodeGraph_Manager_H__
+
+
+class NodeGraph_Manager {
+
+public:
+
+	NodeGraph_Manager();
+	~NodeGraph_Manager();
+
+private: 
+
+};
+
+#endif
