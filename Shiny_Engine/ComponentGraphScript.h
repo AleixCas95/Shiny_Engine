dc00c16fbacb3f41ebd7f920327ade62ab3e1049
AleixCas95 <leix_05@hotmail.com>
2021-01-09 13:58:24 +0100

    Revert "Load & Save ComponentGraphScript"
    This reverts commit 745118bcc86ef92f558e3c9485ff9c4dc2da12aa.

--- Shiny_Engine/ComponentGraphScript.h
+++ Shiny_Engine/ComponentGraphScript.h
@@ -3,26 +3,18 @@
 #define __ComponentGraphScript__H__
 
 #include "Component.h"
-#include <string>
+
 class ComponentGraphScript : public Component
 {
 public:
-	ComponentGraphScript(Application* app_parent, GameObject* parent,uint script_num);
+	ComponentGraphScript(Application* app_parent, GameObject* parent);
 	~ComponentGraphScript();
 
 	void DrawInspector();
-	void Save(JSON_Array* comp_array) const;
-	void Load(JSON_Object* comp_obj);
 
-	//typedef unsigned int scriptType;
-	scriptType scriptNum = 0;
-	scriptType uuid_script;
 private:
-	//const char *uuid_script;
 	bool has_script = false;
-	char script_name[30] = "scripttest";
-	std::vector<GameObject*> gobjects;
-	std::vector<scriptType> uuidsLoad;
+
 };
 
 #endif
