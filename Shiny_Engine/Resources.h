818753e299cd46d9230bcbc538810185663eef86
AitorVelez <aitorvelez@hotmail.com>
2021-01-10 10:54:17 +0100

    Resources update

--- Shiny_Engine/Resources.h
+++ Shiny_Engine/Resources.h
@@ -7,6 +7,7 @@ enum class ResourceType
 	None = 0,
 	Mesh,
 	Texture,
+	Script,
 	Scene
 };
 
@@ -17,11 +18,17 @@ public:
 	virtual ~Resource();
 
 	virtual void Unload() {};
-
+	bool IsLoadedToMemory() const;
+	bool LoadToMemory();
+	const char* GetFile() const;
 	bool operator==(Resource other);
 
 public:
 	std::string name;
+	std::string file;
+	scriptType uuid_1 = 0;
+	uint uuid = 0u;
 	ResourceType type = ResourceType::None;
 	int usage = 0;
+	int loaded = 0;
 };
