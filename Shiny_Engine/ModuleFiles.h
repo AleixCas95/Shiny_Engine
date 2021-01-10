a6851ece3ea702320113fb4918b8c05e6c9a6e57
AleixCas95 <leix_05@hotmail.com>
2021-01-10 17:26:28 +0100

    Module Files / filesystem

--- Shiny_Engine/ModuleFiles.h
+++ Shiny_Engine/ModuleFiles.h
@@ -1,28 +1,84 @@
-#ifndef MODULE_FILES_H
-#define MODULE_FILES_H
+#ifndef __MODULEFILES_H__
+#define __MODULEFILES_H__
 
 #include "Module.h"
-#include "Globals.h"
-#include <map>
-#include <string.h>
+#include <vector>
 
-class Files;
-class ModuleFiles : public Module
+class ConfigEditor;
+
+struct SDL_RWops;
+int close_sdl_rwops(SDL_RWops* rw);
+
+struct aiFileIO;
+
+//struct BASS_FILEPROCS;
+
+enum FileType {
+	File_Unknown,
+	File_Mesh,
+	File_Material,
+	File_Script,
+	File_Scene,
+	File_Meta,
+	No_Extension
+};
+
+class ModuleFileSystem : public Module
 {
 public:
-	ModuleFiles(Application* app, bool start_enabled = true);
-	~ModuleFiles();
 
-	bool Init();
-	update_status PreUpdate(float dt);
-	update_status Update(float dt);
-	update_status PostUpdate(float dt);
-	bool CleanUp();
+	ModuleFileSystem(Application* app, bool start_enabled = true);
 
-	uint SearchFiles(std::string path);
+	// Destructor
+	~ModuleFileSystem();
 
-public:
-	std::map<uint, Files*> resources;
+	// Called before render is available
+	bool Init(ConfigEditor* config);
+
+	// Called before quitting
+	bool CleanUp() override;
+
+	// Utility functions
+	bool AddPath(const char* path_or_zip);
+	bool Exists(const char* file) const;
+	bool IsDirectory(const char* file) const;
+	void CreateDirectory(const char* directory);
+	void DiscoverFiles(const char* directory, std::vector<std::string>& file_list, std::vector<std::string>& dir_list) const;
+	bool CopyFromOutsideFS(const char* full_path, const char* destination);
+	bool Copy(const char* source, const char* destination);
+	void SplitFilePath(const char* full_path, std::string* path, std::string* file = nullptr, std::string* extension = nullptr) const;
+	void NormalizePath(char* full_path) const;
+	void NormalizePath(std::string& full_path) const;
+	FileType DetermineFileType(const char* file_name) const;
+	void CreateAllDirectories();
+
+	// Open for Read/Write
+	unsigned int Load(const char* path, const char* file, char** buffer) const;
+	unsigned int Load(const char* file, char** buffer) const;
+	SDL_RWops* Load(const char* file) const;
+	//void* BassLoad(const char* file) const;
+
+	// IO interfaces for other libs to handle files via PHYSfs
+	aiFileIO* GetAssimpIO();
+	//BASS_FILEPROCS* GetBassIO();
+
+	unsigned int Save(const char* file, const void* buffer, unsigned int size, bool append = false) const;
+	bool SaveUnique(std::string& output, const void* buffer, uint size, const char* path, const char* prefix, const char* extension);
+	bool Remove(const char* file);
+
+	const char* GetBasePath() const;
+	const char* GetWritePath() const;
+	const char* GetReadPaths() const;
+
+private:
+
+	void CreateAssimpIO();
+	
+
+private:
+
+	aiFileIO* AssimpIO = nullptr;
+	
 };
 
 #endif 
