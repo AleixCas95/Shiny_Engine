1f9366069701e0532639568fc3e832c942bb57de
AleixCas95 <leix_05@hotmail.com>
2021-01-10 17:21:42 +0100

    Added config editor

---
+++ Shiny_Engine/ConfigEditor.cpp
@@ -0,0 +1,116 @@
+#include "ConfigEditor.h"
+#include "Parson/parson.h"
+
+
+
+ConfigEditor::ConfigEditor(const char* file_name)
+{
+	root_value = json_parse_file(file_name);
+
+	if (root_value == NULL) {
+		LOG("JSON not found, creating new file");
+		root_value = json_value_init_object();
+	}
+
+	root_object = json_value_get_object(root_value);
+	module_object = root_object;
+}
+
+
+ConfigEditor::ConfigEditor(const char* file_name, const char* module_name) {
+	root_value = json_parse_file(file_name);
+
+	if (root_value == NULL) {
+		LOG("JSON not found, creating new file");
+		root_value = json_value_init_object();
+	}
+
+	root_object = json_value_get_object(root_value);
+	module_object = json_object_get_object(root_object, module_name);
+}
+
+ConfigEditor::~ConfigEditor()
+{
+	//Need to json_value_free
+}
+
+
+void ConfigEditor::SaveFile(const char* file_name) {
+	json_serialize_to_file_pretty(root_value, file_name);
+}
+
+
+ConfigEditor ConfigEditor::GetModuleLocation(const char* name) {
+
+
+	module_object = json_object_get_object(root_object, name);
+
+	return *this;
+}
+
+
+void ConfigEditor::WriteBool(const char* name, bool value) {
+	LOG("Writing bool");
+	json_object_set_boolean(module_object, name, value);
+}
+
+
+bool ConfigEditor::ReadBool(const char* name, bool default) const {
+
+	bool ret = json_object_get_boolean(module_object, name);
+	if (ret) {
+		LOG("%s is true", name);
+	}
+	else {
+		LOG("%s is false", name);
+	}
+
+	return ret;
+
+
+	//return default;
+}
+
+
+void ConfigEditor::WriteInt(const char* name, int value) {
+	LOG("Writing int");
+	json_object_set_number(module_object, name, (double)value);
+}
+
+
+int ConfigEditor::ReadInt(const char* name, int default) const {
+	LOG("Int is %i", (int)json_object_get_number(module_object, name));
+
+	return (int)json_object_get_number(module_object, name);
+
+	//return default;
+}
+
+
+void ConfigEditor::WriteUint(const char* name, uint value) {
+	LOG("Writing uint %i", value);
+	json_object_set_number(module_object, name, (double)value);
+
+}
+
+
+void ConfigEditor::WriteFloat(const char* name, float value) {
+	LOG("Writing float");
+	json_object_set_number(module_object, name, (float)value);
+}
+
+
+float ConfigEditor::ReadFloat(const char* name, float default) const {
+	LOG("Float is %f", (float)json_object_get_number(module_object, name));
+
+	return (float)json_object_get_number(module_object, name);
+
+	//return default;
+}
+
+
+void ConfigEditor::WriteString(const char* name, const char* string) {
+	LOG("Writing string");
+	json_object_set_string(module_object, name, string);
+}
+
---
+++ Shiny_Engine/ConfigEditor.h
@@ -0,0 +1,57 @@
+#ifndef __ConfigEditor__
+#define __ConfigEditor__
+
+#include "Globals.h"
+
+struct json_object_t;
+typedef struct json_object_t JSON_Object;
+
+struct json_value_t;
+typedef struct json_value_t  JSON_Value;
+
+struct json_array_t;
+typedef struct json_array_t  JSON_Array;
+
+
+class ConfigEditor {
+
+public:
+
+	ConfigEditor(const char* file_name);
+	ConfigEditor(const char* file_name, const char* module_name);
+	~ConfigEditor();
+
+
+	ConfigEditor GetModuleLocation(const char* name);
+
+	void WriteBool(const char* name, bool value);
+	bool ReadBool(const char* name, bool default) const;
+
+	void WriteInt(const char* name, int value);
+	int ReadInt(const char* name, int default) const;
+
+	void WriteUint(const char* name, uint value);
+
+
+	void WriteFloat(const char* name, float value);
+	float ReadFloat(const char* name, float default) const;
+
+	void WriteString(const char* name, const char* string);
+
+
+
+	void SaveFile(const char* file_name);
+public:
+
+	JSON_Value* root_value = nullptr;
+	JSON_Object* root_object = nullptr;
+	JSON_Object* module_object = nullptr;
+
+
+
+};
+
+
+
+
+#endif // __CONFIG_EDITOR__
